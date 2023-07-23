11a90f14a36f4c1bf5e0606bf02b701fb2f2cd14
vinitborad <vinitborad12@gmail.com>
2023-07-23 20:52:06 +0530

    Commiting whole studynotion project

---
+++ server/controllers/ContactUs.js
@@ -0,0 +1,33 @@
+const { contactUsEmail } = require("../mail/templates/contactFormRes")
+const mailSender = require("../utils/mailSender")
+
+exports.contactUsController = async (req, res) => {
+  const { email, firstname, lastname, message, phoneNo, countrycode } = req.body
+  console.log("contactUsController printing")
+  console.log(req.body)
+  try {
+    const emailRes = await mailSender(
+      "vinittrying2004+feedback@gmail.com",
+      "Your Data send successfully",
+      contactUsEmail(email, firstname, lastname, message, phoneNo, countrycode)
+    )
+    await mailSender(
+      email,
+      "Your Data send successfully",
+      contactUsEmail(email, firstname, lastname, message, phoneNo, countrycode)
+    )
+    console.log("Email Res ", emailRes)
+    return res.json({
+      success: true,
+      data:emailRes,
+      message: "Email send successfully",
+    })
+  } catch (error) {
+    console.log("Error", error)
+    console.log("Error message :", error.message)
+    return res.json({
+      success: false,
+      message: "Something went wrong...",
+    })
+  }
+}
